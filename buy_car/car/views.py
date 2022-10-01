e2e6a9f92abfc0f3dd0b4e19706ad6b3256e97e0
AleksandrPrishepov <sanyaprishepov89@gmail.com>
2022-10-01 12:11:40 +0300

    created view for url about

--- buy_car/car/views.py
+++ buy_car/car/views.py
@@ -133,8 +133,13 @@ def logout_user(request):
     logout(request)
     return redirect('main')
 
-class About(ListView):
-    pass
+class About(DataMixin, TemplateView):
+    template_name = 'car/about_site.html'
+    hard_skils = ('Python', 'Django', 'ORM Django', 'СУБД: SQLite')
+    def get_context_data(self,*, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title='Главная страница', hard_skils=self.hard_skils)
+        return dict(list(context.items()) + list(c_def.items()))
 
 class Connect(FormView):
     pass
